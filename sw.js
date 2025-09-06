ce09666d018ee01c06384ce39240fe86ba2bfa94
theviperyt <166864651+theviperyt@users.noreply.github.com>
2025-09-06 05:33:24 +0530

    Update sw.js
    added offline support

--- sw.js
+++ sw.js
@@ -1,17 +1,49 @@
 // sw.js
-self.addEventListener("install", e => {
-    e.waitUntil(
-      caches.open("static").then(cache => {
-        return cache.addAll(["./", "./index.html", "./favicon.png"]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", e => {
-    e.respondWith(
-      caches.match(e.request).then(response => {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+const CACHE_NAME = "my-site-cache-v1";
+
+// Add all the files you want available offline
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/favicon.png"
+  // add more files (images, fonts, etc.) here
+];
+
+// Install event - cache everything listed
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Pre-caching offline assets");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event - cleanup old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve cached content when offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
